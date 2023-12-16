97ca1b812c15ef3e65b12ecdcb6efb92d0eb26c2
Anderson Moreira <anderson.tecnicoprogramdor@hotmail.com>
2023-12-16 18:40:46 -0300

    intruduçãologica de programação

---
+++ Introdução a logica de Programação/Description.md
@@ -0,0 +1,3 @@
+Acesse link abaixo com descrição da explicando fluxograma do site!
+
+https://helpful-jump-17b.notion.site/Mapa-de-aventura-91f3e9bd923842149d4dba754dc65c07?p=fd3cb94523b142d08b983d456e94668c&pm=c
---
+++ Trabalhando com variáveis/Tipos variáveis.js
@@ -0,0 +1,47 @@
+// `string` - são variaveis que armazenam texto
+
+// **`number` - são variaveis que armazenam números**
+
+// `boolean` - são variaveis lógicas, que armazenam o valor de ligado ou desligado (true/false)
+
+
+//pokemon
+let nomePokemon = "pikachu"
+let pokemonSexo = "M"
+
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+
+let selecionavel = false
+
+
+
+
+// Outros tipos de variáveis
+
+// Tipo de Variável
+
+// Inteiro
+
+// Ponto flutuante ou decimal
+
+// Números complexos
+
+// Variáveis de texto
+
+// String: Armazena uma sequência de caracteres, como "Olá, mundo!"
+
+
+// Caractere: Armazena um único caractere, como 'a', 'X', '@'.
+
+
+// Variáveis lógicas: Usadas para armazenar valores de verdadeiro ou falso.
+
+
+// Booleano: Armazena os valores True ou False.
+
+// Variáveis de data e hora: Utilizadas para representar datas e horários.
+
+// Data: Armazena datas, no formato AAAA-MM-DD.
+
+// Hora: Armazena horários, no formato HH:MM:SS.
